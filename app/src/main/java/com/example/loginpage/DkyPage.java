ae58b4302c7ceb9b6f86afd9c5134b0965cb948a
ThanhSangNguyen <thanhsang.nguyen.vtp@gmail.com>
2022-03-15 16:36:59 +0700

    BOXYZVN Day2

---
+++ app/src/main/java/com/example/loginpage/DkyPage.java
@@ -0,0 +1,74 @@
+package com.example.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class DkyPage extends AppCompatActivity {
+    Button dky2;
+    EditText ho,ten,dob,pass,copass;
+    CheckBox checkBox;
+    RadioButton nam,nu;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dky_page);
+
+        dky2 = findViewById(R.id.dky2);
+        ho = findViewById(R.id.editTextHo);
+        ten = findViewById(R.id.editTextTen);
+        dob = findViewById(R.id.editTextDOB);
+        pass = findViewById(R.id.editTextPass);
+        copass = findViewById(R.id.editTextCoPass);
+        checkBox = findViewById(R.id.checkBox);
+        nam = findViewById(R.id.nam);
+        nu = findViewById(R.id.nu);
+
+        nam.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                dky2.setEnabled(true);
+                if(b){
+                    Toast.makeText(DkyPage.this, "Giới tính: Nam ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        nu.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                dky2.setEnabled(true);
+                if(b){
+                    Toast.makeText(DkyPage.this, "Giới tính: Nữ ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+        dky2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String dk1 = ho.getText().toString().trim();
+                String dk2 = ten.getText().toString().trim();
+                String dk3 = dob.getText().toString().trim();
+                String dk4 = pass.getText().toString().trim();
+                String dk5 = copass.getText().toString().trim();
+                Boolean b = !checkBox.isChecked();
+
+                if (dk1.isEmpty() || dk2.isEmpty() || dk3.isEmpty() || dk4.isEmpty() || dk5.isEmpty() || b ) {
+                    Toast.makeText(DkyPage.this, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(DkyPage.this, "Đăng ký thành công!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button bt1, bt2;
+    EditText tk, mk;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt1 = findViewById(R.id.login);
+        bt2 = findViewById(R.id.dky);
+        tk = findViewById(R.id.tk);
+        mk = findViewById(R.id.mk);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str1 = tk.getText().toString().trim();
+                String str2 = mk.getText().toString().trim();
+                if (str1.isEmpty() || str2.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Chào mừng quay trở lại, " + str1, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DkyPage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
